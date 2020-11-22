cd57fd14c0df11c41eab3bf07899aa7bc293e19d
Valdas Rakutis <valdas@rakutis.lt>
2020-11-22 19:11:49 +0200

    Generate product vector rows

--- Codes/Communication/Alternative/GeneratorMatrix.cs
+++ Codes/Communication/Alternative/GeneratorMatrix.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Codes.Infrastructure;
 using Codes.Primitives;
 
 namespace Codes.Communication.Alternative
@@ -15,10 +16,38 @@ namespace Codes.Communication.Alternative
         {
             R = r;
             M = m;
-            _rows = GetStartingVectors().ToList();
+            Generate();
         }
 
-        private IEnumerable<IndexedVector> GetStartingVectors()
+        /// <summary>
+        /// Generates the matrix rows using R and M parameters.
+        /// Stores the result in _rows;
+        /// </summary>
+        private void Generate()
+        {
+            var baseVectors = GetBaseVectors().ToList();
+            // if R <= 1, then there is no need to generate the products from vector combinations
+            if (R <= 1)
+            {
+                _rows = baseVectors;
+                return;
+            }
+            //get all possible combinations from the base vectors in collections of size 2 - R.
+            //skip the first 'base' vector, as it's equivalent to 1, and when multiplying by it later, the resulting vector is simply itself.
+            var combinations = baseVectors.Skip(1).GetCombinations(2, R);
+
+            var indexedCombinedVectors = combinations
+                .Select(combination => IndexedVector.Combine(combination, VectorSize));
+
+            _rows = baseVectors.Concat(indexedCombinedVectors).ToList();
+        }
+
+        /// <summary>
+        /// Builds vectors which are used to generate the rest of the vectors,
+        /// for example: 11111111, 11110000, 11001100, 10101010 for M = 3;
+        /// </summary>
+        /// <returns> indexed vectors </returns>
+        private IEnumerable<IndexedVector> GetBaseVectors()
         {
             var index = 1; //indexed used to index vectors;
             //Starting from 2**M, go down in powers of two
@@ -40,7 +69,7 @@ namespace Codes.Communication.Alternative
                 }
                 yield return new IndexedVector
                 {
-                    Index = new HashSet<int>{ index },
+                    Index = new HashSet<int> { index },
                     Value = new Vector(vectorBits)
                 };
             }
@@ -51,6 +80,28 @@ namespace Codes.Communication.Alternative
     {
         public HashSet<int> Index { get; set; }
         public Vector Value { get; set; }
+        
+        /// <summary>
+        /// Combines vectors into their product vector.
+        /// for example: [ v1, v2 ] => v3: {Value:v1 * v2, Index:[v1.Index, v2.Index]
+        /// </summary>
+        /// <param name="vectors">vectors to combine</param>
+        /// <param name="vectorSize">the size of the passed vectors</param>
+        /// <returns>combined vector</returns>
+        public static IndexedVector Combine(IEnumerable<IndexedVector> vectors, int vectorSize)
+        {
+            var result = new IndexedVector
+            {
+                Index = Enumerable.Empty<int>().ToHashSet(),
+                Value = Vector.One(vectorSize)
+            };
+            foreach (var vector in vectors)
+            {
+                result.Value = result.Value.Multiply(vector.Value);
+                result.Index.UnionWith(vector.Index);
+            }
+            return result;
+        }
 
         #region object
         public override int GetHashCode()
